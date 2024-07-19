e41a0c7a0403f1bc755cf04a1437bcb67dea3198
sandrabanchiri <sandrabanchiri39@gmail.com>
2024-07-19 21:31:29 +0300

    example of javascript events

---
+++ index.js
@@ -0,0 +1,11 @@
+// javascrip events 
+//mouse click 
+//key press 
+// keydown ,keyup and keypress events
+//when a key is pressed this events provide a code to indicate which key it Was .
+//form submission
+//scroll
+//mouseenter
+//mouseleave
+//focusblur
+//onchange
